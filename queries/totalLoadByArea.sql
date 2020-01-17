fd3c16f7e8eba5e4eaaa07d1f5043f1ab6738805
nox491 <57879123+nox491@users.noreply.github.com>
2020-01-17 18:53:44 +0200

    Update totalLoadByArea.sql

--- queries/totalLoadByArea.sql
+++ queries/totalLoadByArea.sql
@@ -1,3 +1,3 @@
-SELECT '' AS Source, '' AS Datasate, AreaName, AreaTypeCodeText, MapCodeText, ResolutionCodeText, Year, Month, Day, DateTime, TotalLoadValue, UpdateTime 
+SELECT 'entso-e' AS Source, 'ActualTotalLoad' AS Dataset, AreaName, AreaTypeCodeText, MapCodeText, ResolutionCodeText, Year, Month, Day, DateTime, TotalLoadValue, UpdateTime 
 FROM ActualTotalLoad, ResolutionCode, AreaTypeCode, MapCode
 WHERE AreaName='Greece' AND ResolutionCodeText='PT60M' AND ResolutionCode.Id = ResolutionCodeId AND MapCode.Id=MapCodeId AND AreaTypeCode.Id=AreaTypeCodeId;
