03a8eb30dd490ff48a7f34757cd33009ee6b7f0f
nox491 <57879123+nox491@users.noreply.github.com>
2020-01-19 21:58:04 +0200

    Delete totalLoadByYear.sql

--- queries/totalLoadByYear.sql
+++
@@ -1,3 +0,0 @@
-SELECT '' AS Source, '' AS Dataset, AreaName, AreaTypeCodeText, MapCodeText, ResolutionCodeText,Year, Month, SUM(TotalLoadValue) AS ActualTotalLoadByDateValue
-FROM ActualTotalLoad, ResolutionCode, AreaTypeCode, MapCode
-WHERE AreaName='Greece' AND ResolutionCodeText='PT60M' AND ResolutionCode.Id = ResolutionCodeId AND MapCode.Id=MapCodeId AND AreaTypeCode.Id=AreaTypeCodeId AND ResolutionCodeId=2 AND Year=2018;
